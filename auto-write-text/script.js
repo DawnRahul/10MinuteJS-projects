450607ef3da1b0fcd38dc6092947ed1612d6293a
Rahul Kumar <45137578+DawnRahul@users.noreply.github.com>
2021-02-21 17:44:52 +0530

    Add auto-write-text project files

---
+++ auto-write-text/script.js
@@ -0,0 +1,39 @@
+
+const text = "Hi! I'm Rahul a Web Developer...";
+
+let index = 0;
+
+function autoText(){
+    
+    var textIncreementation = setInterval(() => {
+        document.getElementById('auto-text').innerHTML = text.slice(0, index);
+        index++;
+        console.log(index);
+
+        //when complete text is displayed
+        if(index > text.length){
+            index--;
+            //textDecreementation
+            var textDecreementation = setInterval(() => {
+                document.getElementById('auto-text').innerHTML = text.slice(0, index);
+                index--;
+                console.log(index);
+
+                if(index == 0){
+                    autoText(); 
+                    //stop textDecreement
+                    clearInterval(textDecreementation);
+                }
+
+            }, 50);
+
+            clearInterval(textIncreementation);
+        }
+    }, 100);
+
+    
+    
+}
+
+//first call of autoText()
+autoText();
