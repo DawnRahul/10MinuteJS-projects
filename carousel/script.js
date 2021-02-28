e8475e6cc21956724a1ee7891f03a6baa87467ca
Rahul Kumar <45137578+DawnRahul@users.noreply.github.com>
2021-02-28 15:33:54 +0530

    Add load event at line 10

--- carousel/script.js
+++ carousel/script.js
@@ -6,37 +6,38 @@ const imgContainer2 = document.getElementsByClassName("carousel-image")[1];
 const imgContainer3 = document.getElementsByClassName("carousel-image")[2];
 const imgContainer4 = document.getElementsByClassName("carousel-image")[3];
 
-//get the width of carousel for making carousel responsive to different screen sizes
-var carouselWidth = window.getComputedStyle(document.querySelector(".carousel")).width;
-console.log(carouselWidth); //getComputedStyle() returns correct value only when server is live means otherwise it will return default values
-carouselWidth = parseInt(carouselWidth);   //parseInt() because before carouselWidth was a string type and i need it as integer
+//load event used to prevent javascript to calculate width before actual width is given by CSS to webpage
+window.addEventListener("load", ()=>{
+    //get the width of carousel for making carousel responsive to different screen sizes
+    var carouselWidth = window.getComputedStyle(document.querySelector(".carousel")).width;
+    console.log(carouselWidth); //getComputedStyle() returns correct value only when server is live means otherwise it will return default values
+    carouselWidth = parseInt(carouselWidth);   //parseInt() because before carouselWidth was a string type and i need it as integer
 
-//making array of image elements
+
+
+    //making array of image elements
 let imgArray = [imgContainer1, imgContainer2, imgContainer3, imgContainer4];
 
 imgArray[0].style.position = "relative";
 imgArray[0].style.visibility = "hidden";
 
-// setting position as absolute except for the first image for all images
-for(let i=1 ; i <= imgArray.length-1; i++){
-    imgArray[i].style.position = "absolute";
-}
 
-// setting initial position for all images
-for(let i=-1 ; i <= imgArray.length-3; i++){
-    imgArray[i+2].style.transform = `translateX(${ carouselWidth * i}px)`;
-    // imgArray[i+2].style.transform = `translateX(${600 * i}px)`;
-    
-    //initially images will slide at z-index of -5, except image which have to display first
-    if(i !== 0){
-        imgArray[i+2].style.zIndex = -2;
+    // setting position as absolute except for the first image for all images
+    for(let i=1 ; i <= imgArray.length-1; i++){
+        imgArray[i].style.position = "absolute";
     }
     
-}
-
-
-
-
+    // setting initial position for all images
+    for(let i=-1 ; i <= imgArray.length-3; i++){
+        imgArray[i+2].style.transform = `translateX(${ carouselWidth * i}px)`;
+        // imgArray[i+2].style.transform = `translateX(${600 * i}px)`;
+        
+        //initially images will slide at z-index of -5, except image which have to display first
+        if(i !== 0){
+            imgArray[i+2].style.zIndex = -2;
+        }
+        
+    }
 
 
 
@@ -64,3 +65,7 @@ imgArray[index].style.transform = tempVar;
 
 
 }, 3000);
+
+
+});
+
